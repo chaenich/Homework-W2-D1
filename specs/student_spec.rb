3ca25db3e70b54c2b0408ebaf1306421a5722f77
Chae Nicholson <chae.nicholson@gmail.com>
2019-05-20 14:20:32 +0100

    Student setup complete

---
+++ specs/student_spec.rb
@@ -0,0 +1,14 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../student')
+
+class TestStudent < MiniTest::Test
+
+  # def test_student_name
+  #   student = Student.new('Chae Nich', 'E31')
+  #
+  #   assert_equal('Chae Nich', student.get_name)
+  # end
+
+
+end
---
+++ student.rb
@@ -0,0 +1,18 @@
+class Student
+
+# attr_reader( :student_name, :student_cohort)
+# attr_accessor( :holder_name)
+
+  def initialize(name, cohort)
+    @student_name = name
+    @student_cohort = cohort
+  end
+
+  # def get_name()
+  #   return student_name
+  # end
+  #
+  # def get_cohort()
+  #   return student_cohort
+  # end
+end
