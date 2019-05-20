e82ece121353b52240e54cc05f299b9ce46bd61a
Chae Nicholson <chae.nicholson@gmail.com>
2019-05-20 15:21:05 +0100

    team test setup complete

---
+++ team.rb
@@ -0,0 +1,11 @@
+class Team
+
+# attr_accessor( :student_name, :student_cohort)
+
+  def initialize(name, players, coach)
+    @team_name = name
+    @team_players = players[]
+    @team_coach = coach
+  end
+
+end
