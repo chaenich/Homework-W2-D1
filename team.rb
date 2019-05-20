d877decadfa2d87a86b198984fb84a061733264d
Chae Nicholson <chae.nicholson@gmail.com>
2019-05-20 16:46:44 +0100

    Points property added

--- team.rb
+++ team.rb
@@ -3,10 +3,11 @@ class Team
 attr_reader( :team_name, :team_players )
 attr_accessor( :team_coach )
 
-  def initialize(name, players, coach)
+  def initialize(name, players, coach, points)
     @team_name = name
     @team_players = players
     @team_coach = coach
+    @points = 0
   end
 
   def add_player(new_player)
