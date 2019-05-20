e4c3147eae012015334799457ade3e72c9bef655
Chae Nicholson <chae.nicholson@gmail.com>
2019-05-20 17:27:39 +0100

    Points update after result completed. Part B completed

--- team.rb
+++ team.rb
@@ -1,21 +1,33 @@
 class Team
 
-attr_reader( :team_name, :team_players )
-attr_accessor( :team_coach )
+  attr_reader( :team_name, :team_players )
+  attr_accessor( :team_coach, :team_points )
 
-  def initialize(name, players, coach, points)
+  def initialize(name, players, coach, points = 0)
     @team_name = name
     @team_players = players
     @team_coach = coach
-    @points = 0
+    @team_points = points
   end
 
   def add_player(new_player)
-    team_players.push(new_player)
+    @team_players.push(new_player)
   end
 
   def check_for_player(player)
-    return team_players.include?(player)
+    return @team_players.include?(player)
   end
 
+  def update_points(result)
+    # win: +3 points
+    # draw: +1 point
+    # lose: 0 points
+    if result == 'win'
+      @team_points += 3
+    elsif result == 'draw'
+      @team_points += 1
+    else
+      @team_points += 0
+    end
+  end
 end
